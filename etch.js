8141b6ae9b5167c529f0adda915affb3103ac86a
ooharu <jitender032000kumar@gmail.com>
2025-05-16 01:31:19 +0530

    dyanamic squares

---
+++ etch.js
@@ -0,0 +1,39 @@
+
+const container = document.querySelector("#container")
+
+const btn = document.createElement("button")
+btn.textContent = "Number of squares"
+document.body.appendChild(btn)
+
+for (let i=0; i<16; i++)
+{
+    for( let j=0 ;j<16; j++)
+    {
+        const content = document.createElement("div")
+        content.classList.add("square")
+        container.appendChild(content)
+    }
+}
+
+btn.addEventListener("click", ()=>{
+    const squares = prompt("Enter the new squares : ")
+    container.innerHTML = ""
+
+    const gridSize = 960;
+    container.style.width = gridSize + "px";
+    container.style.height = gridSize + "px";
+    const squareSize = gridSize / squares;
+    
+    for (let i=0; i<squares; i++)
+        {
+            for( let j=0 ;j<squares; j++)
+            {
+                const content = document.createElement("div")
+                content.classList.add("square")
+                content.style.width = squareSize + "px";
+                content.style.height = squareSize + "px";        
+                container.appendChild(content)
+            }
+        }
+    
+})
